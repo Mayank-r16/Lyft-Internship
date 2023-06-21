e8b611249a3b04c364737b5765dc990836c4eed2
Mayank Rajput <111078589+Mayank-r16@users.noreply.github.com>
2023-06-21 20:24:39 +0530

    Update sternman_engine.py

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,15 +1,10 @@
-from abc import ABC
+from engine import Engine
 
-from car import Car
+class CapuletEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
+        super().__init__(current_mileage, last_service_mileage)
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
 
-
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
-        self.warning_light_is_on = warning_light_is_on
-
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
+    def engine_needs_service(self):
+        return self.current_mileage - self.last_service_mileage > 30000
